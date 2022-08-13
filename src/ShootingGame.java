2a55a4613e9afd4e30f6074fceeac1a80d90ec46
지민 백 <qorwlalsjimin@naver.com>
2022-08-13 19:58:03 +0900

    ShootingGame, main 클래스 생성

---
+++ src/ShootingGame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class ShootingGame extends JFrame {
+    public ShootingGame(){
+        setTitle("Shooting Game");
+        setUndecorated(true);
+        setSize(1280, 720);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+    }
+}
